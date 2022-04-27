1c24c63698867133c9f101431251ef205417fe3e
sachin2022apr-cedcommerce <sachin2022apr@cedcoss.com>
2022-04-27 15:43:13 +0530

    Counter Task

---
+++ script.js
@@ -0,0 +1,7 @@
+var i = 0;
+    function buttonplus() {
+        document.getElementById('inc').innerHTML= ++i;
+    }
+    function buttonminus() {
+        document.getElementById('inc').innerHTML = --i;
+    }
